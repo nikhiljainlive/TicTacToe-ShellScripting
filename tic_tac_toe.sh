db562a695530e96823fdc8924f0c0e241f3e79a8
nikhiljainlive <nikhiljain5195@gmail.com>
2020-02-03 13:13:13 +0530

    [Nikhil]Refactor. initializing Board in tic_tac_toe.sh file

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -2,4 +2,17 @@
 
 #This file contains Tic-Tac-Toe Game logic
 
+ROWS=3
+COLUMNS=3
+INITIAL_SYMBOL="-"
 declare -A board
+
+initBoard() {
+	for (( row=0; row < ROWS; row++ )) do
+		for (( column=0; column < COLUMNS; column++ )) do
+			board[$row, $col]=$INITIAL_SYMBOL
+		done
+	done	
+}
+
+initBoard

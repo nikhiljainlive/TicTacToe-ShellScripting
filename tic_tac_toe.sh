9b32bc415c527f8470e65a9747c3219490129c90
nikhiljainlive <nikhiljain5195@gmail.com>
2020-02-03 16:20:51 +0530

    [Nikhil]Refactor. printBoard function added to tic_tac_toe.sh file

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -8,11 +8,26 @@ INITIAL_SYMBOL="-"
 declare -A board
 
 initBoard() {
-	for (( row=0; row < ROWS; row++ )) do
-		for (( column=0; column < COLUMNS; column++ )) do
-			board[$row, $col]=$INITIAL_SYMBOL
+	for (( row = 0; row < ROWS; row++ )); do
+		for (( column = 0; column < COLUMNS; column++ )); do
+			board[$row,$column]=$INITIAL_SYMBOL
 		done
-	done	
+	done
+}
+
+printBoard() {
+	printf "\n-------------------"
+	for (( row = 0; row < ROWS; row++ )) do
+		printf "\n|     |     |     |\n"
+		for (( column = 0; column < COLUMNS; column++ )) do 
+			printf "|  ${board[$row,$column]}  "
+		done
+	printf "|"
+	printf "\n|     |     |     |"
+	printf "\n-------------------"
+	done
+	printf "\n\n"
 }
 
 initBoard
+printBoard
